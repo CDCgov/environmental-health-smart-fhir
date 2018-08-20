f0c29fc638f585a8c3dd24942f4c8b9b60a35899
Hochwarter <nto1@cdc.gov>
2018-08-20 18:56:45 -0400

    added patient info tab for sending info material to patient

--- src/components/AirQualityInfo.vue
+++ src/components/AirQualityInfo.vue
@@ -2,12 +2,14 @@
   <div>
     <div class="tabs is-centered">
       <ul>
-        <li :class="{ 'is-active': view === 'summary' }" @click="toggleView"><a>Air Quality Indicators (AQI)</a></li>
-        <li :class="{ 'is-active': view === 'daily' }" @click="toggleView"><a>Daily AQI for Encounters</a></li>
+        <li :class="{ 'is-active': view === 'summary' }" @click="toggleView('summary')"><a>Air Quality Indicators (AQI)</a></li>
+        <li :class="{ 'is-active': view === 'daily' }" @click="toggleView('daily')"><a>Daily AQI for Encounters</a></li>
+        <li :class="{ 'is-active': view === 'patientinfo' }" @click="toggleView('patientinfo')"><a>Inform Patient</a></li>
       </ul>
     </div>
     <p-m25-visualization v-if="view === 'summary'" :patient="patient"></p-m25-visualization>
     <encounters-air-quality :encounters="encounters" v-else-if="view === 'daily'" :patient="patient" ></encounters-air-quality>
+    <patient-info v-else-if="view==='patientinfo'"></patient-info>
   </div>
 
 </template>
@@ -16,13 +18,15 @@
 import PM25Visualization from './PM25Visualization';
 import AirNow from './AirNow';
 import EncountersAirQuality from './EncountersAirQuality';
+import PatientInfo from './PatientInfo';
 
 export default {
   name: 'air-quality-info',
   components: {
     AirNow,
     PM25Visualization,
-    EncountersAirQuality
+    EncountersAirQuality,
+    PatientInfo
   },
   props: {
     patient: {
@@ -38,8 +42,8 @@ export default {
     };
   },
   methods: {
-    toggleView() {
-      this.view = this.view === 'summary' ? 'daily' : 'summary';
+    toggleView(view) {
+      this.view = view;
     }
   }
 };
---
+++ src/components/PatientInfo.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container">
+    <h1 class="title is-5">Prepare and send information to patient</h1>
+    <div class="field">
+      <label class="label">Title</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Information title (optional)">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Message</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Message"></textarea>
+      </div>
+    </div>
+    <hr>  
+    <div class="field">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox">
+          Send Air Quality and Asthma <strong>fact sheets</strong>
+        </label>
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox">
+          Send <strong>alerts</strong> on elevated Air Quality Indicator values
+        </label>
+      </div>
+    </div>
+    <hr>
+    <div class="field">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox">
+          Send to email
+        </label>
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <label class="checkbox">
+          <input type="checkbox">
+          Send to mobile phone
+        </label>
+      </div>
+    </div>
+
+    <hr>    
+    <div class="field">
+      <div class="control is-danger">
+        <label class="checkbox">
+          <input type="checkbox" class="is-danger">
+          I have the patient's consent to send this information.
+        </label>
+      </div>
+    </div>
+
+
+  <div class="field is-grouped">
+    <div class="control">
+      <button class="button is-link">Send</button>
+    </div>
+    <div class="control">
+      <button class="button">Print handout</button>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'patient-info',
+}
+</script>
+
+<style>
+  div.is-danger {
+    color: #ff3860;
+  }
+</style>
